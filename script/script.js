198662b5644e9040a2f8a58395b1815d27bf568c
fabiolafisca <fabiolafisca@hotmail.com>
2024-10-29 17:47:19 +0100

    esercizio FizzBuzz

--- script/script.js
+++ script/script.js
@@ -7,3 +7,21 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 "use strict"
 console.clear()
 
+for (let i=1; i <= 100; i++){
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz");
+    }
+        
+    else if (i % 3 === 0){
+        console.log("Fizz");
+    }
+        
+    else if (i % 5 === 0){
+        console.log("Buzz");
+    }
+        
+    else{
+        console.log(i);
+    }
+}
+
